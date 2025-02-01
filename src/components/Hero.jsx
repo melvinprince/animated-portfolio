aa377a6ca01521b682c488090537a022f8b11f5a
Melvin <melvinmps11301@gmail.com>
2025-02-01 14:48:22 +0300

    navigation created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,3 @@
+export default function Hero() {
+  return <div className="hero">Ho Hero</div>;
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import "./styles/sidebar.scss";
+import { a, clipPath } from "framer-motion/client";
+import { stagger } from "framer-motion/dom";
+
+export default function Sidebar() {
+  const [open, setOpen] = useState(false);
+  const variants = {
+    open: {
+      clipPath: "circle(120rem at 5rem 5rem)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        restDelta: 2,
+      },
+    },
+    closed: {
+      clipPath: "circle(3rem at 5rem 5rem)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+}
+
+//LINKS
+function Links() {
+  const variants = {
+    open: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+    closed: {
+      transition: {
+        staggerChildren: 0.05,
+        staggerDirection: -1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    open: {
+      y: 0,
+      opacity: 1,
+    },
+    closed: {
+      y: 50,
+      opacity: 0,
+    },
+  };
+
+  const items = ["Home", "About", "Services", "Projects", "Contact"];
+  return (
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a
+          href="#"
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+}
+
+//TOGGLE BUTTON
+function ToggleButton({ setOpen }) {
+  return (
+    <button className="button" onClick={() => setOpen((prev) => !prev)}>
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          d="M 2 9.423 L 20 9.423"
+          variants={{
+            closed: { opacity: 1 },
+            open: { opacity: 0 },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 16.346 L 20 16.346" },
+            open: { d: "M 3 2.5 L 17 16.346" },
+          }}
+        />
+      </svg>
+    </button>
+  );
+}
